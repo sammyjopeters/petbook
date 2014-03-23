ea267404bbfe0d3ca6ff268379717444b0a70dbf
sammyjopeters <peters.sammyjo@gmail.com>
2014-03-23 17:46:38 +1100

    devise installed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Petbook::Application.config.secret_key_base = '7459cee0e5f089e8be9e076cb88bf129b8864124bd644be95be4ee2b7ca5710980e9c5079c6671604cbd255ebb5f2834902fa5e9a7740e8155b0ce1635ded850'
+Petbook::Application.config.secret_key_base = '22b12b76f18eb0f9682b25ab81d872bceecd94eda4a54eb1ef09751fc12122abb1929b886a6ef75e39f892fa4d54b28d7ab6e82eb6d7f500bb595fd3216aefd0'
