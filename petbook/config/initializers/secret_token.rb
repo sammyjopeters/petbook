e92d3dd40cd0312036a78ae30225fb0bc293c825
sammyjopeters <peters.sammyjo@gmail.com>
2014-03-23 17:12:58 +1100

    inital commit

---
+++ petbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petbook::Application.config.secret_key_base = '83330bea970e4c9afd5389ca58b1dce12fae23acccc4313dc3de329a9e641ba593d62c596370c04c7b1699004760b515807684dfce7c59621064414c31065ce2'
