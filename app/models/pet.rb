9fe2bef3c248b9206bd45ed8c34117e108a223ad
sammyjopeters <peters.sammyjo@gmail.com>
2014-03-25 15:58:59 +1100

    style and layout changes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,11 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
+
+
+  def age
+    dob = self.dob.to_date
+    currentyear = Time.now.to_date
+    currentyear.year - dob.year - ((currentyear.month > dob.month || (currentyear.month == dob.month && currentyear.day >= dob.day)) ? 0 : 1)
+  end
+
 end
